007961c7727099ed3e207a0baab2a7d92f2dad07
agusev <1990agusev@gmail.com>
2022-11-19 21:36:25 +0100

    feat(auth): add login, registration, lobby views

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const sessionInstance = require('./app-config/session');
+const protect = require('./app-config/protect');
 
-if(process.env.NODE_ENV === 'development') {
-  require("dotenv").config();
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config();
 }
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const testsRouter = require('./routes/tests');
+const indexRouter = require('./routes/pages/index');
+const usersRouter = require('./routes/pages/users');
+const testsRouter = require('./routes/pages/tests');
+const authRouter = require('./routes/pages/auth');
+const lobbyRouter = require('./routes/pages/lobby');
 
 const app = express();
 
@@ -23,25 +27,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(sessionInstance);
 
+// public
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tests', testsRouter);
+app.use('/auth', authRouter);
+
+// protected
+app.use('/lobby', protect, lobbyRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
